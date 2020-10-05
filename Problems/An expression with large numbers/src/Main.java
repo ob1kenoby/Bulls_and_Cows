98cba011b3d15a06b320dfc810d78c89de3f29c1
Sergey Mikhaylov <bodmode@gmail.com>
2020-10-05 12:02:46 +0300

    An expression with large numbers

---
+++ Problems/An expression with large numbers/src/Main.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] numbers = scanner.nextLine().split(" ");
+        BigInteger a = new BigInteger(numbers[0]).negate();
+        BigInteger b = new BigInteger(numbers[1]);
+        BigInteger c = new BigInteger(numbers[2]);
+        BigInteger d = new BigInteger(numbers[3]);
+        System.out.println(a.multiply(b).add(c).subtract(d));
+    }
+}
